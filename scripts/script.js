bc7a5830a3ed944dc7301c5522344b0efbeb6cbf
JuanJUribe <juanjouribev1@gmail.com>
2019-08-08 14:47:38 -0700

    Image Background added

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,11 @@ const recipes = document.querySelectorAll('.recipeResults');
 
 recipes.forEach(r =>
   r.addEventListener('click', e =>{
-  r.style.backgroundColor = 'red';
-}));
+  recipes.forEach(function(element, index){
+    if (element === r){
+      element.style.order = 1;
+    }
+    else{
+      element.style.order += 1;
+    }
+  })}));
